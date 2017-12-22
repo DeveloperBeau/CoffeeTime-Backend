1041ce74b5bd51d7a31e10600da214d40a6eab20
Beau Ayres <beau@domesticcat.com.au>
2017-12-22 14:06:00 +1100

    added web-api

---
+++ api/web-api.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"CoffeeTime-Go/db"
+	"flag"
+	"net/http"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
+
+var s serviceHandler
+
+func Run(endpoint string, db db.Handler) error {
+	r := RunAPIOnRouter(db)
+	return http.ListenAndServe(endpoint, r)
+}
+
+func RunAPIOnRouter(db db.Handler) http.Handler {
+	r := mux.NewRouter()
+	e := MakeServerEndpoints(s, db)
+	flag.Parse()
+
+	r.Methods("POST").Path("/addUser/{webAddress}").Handler(httptransport.NewServer(
+		e.PostWebAddressEndpoint,
+		decodePostWebAddressRequest,
+		encodeResponse,
+	))
+
+	return r
+}
