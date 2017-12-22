b3a7df9a6690ce212183b3ffd0ff7c5dd4383629
Beau Ayres <beau@domesticcat.com.au>
2017-12-22 14:03:54 +1100

    updated main

--- main.go
+++ main.go
@@ -1,3 +1,29 @@
 package main
 
-func main() {}
+import (
+	"CoffeeTime-Go/api"
+	"CoffeeTime-Go/db"
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type configuration struct {
+	ServerAddress string `json:"webserver"`
+}
+
+func main() {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	connectionString := "user=postgres dbname=coffeetime sslmode=disable"
+	handler, dbErr := db.MakeDatabaseHandler(connectionString)
+	if dbErr != nil {
+		log.Fatal(err)
+	}
+	config := new(configuration)
+	json.NewDecoder(file).Decode(config)
+	log.Println("Starting web server on address ", config.ServerAddress)
+	api.Run(config.ServerAddress, handler)
+}
