9d214f25693460b02ecee00a4c061bf7ac25a8d7
Beau Ayres <beau@domesticcat.com.au>
2018-01-09 00:36:50 +1100

    added start session and session functions

--- db/sqlhandler.go
+++ db/sqlhandler.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"CoffeeTime-Go/api"
 )
 
 //SQLHandler : Generic DB handler struct
@@ -16,6 +17,9 @@ type SQLHandler struct {
 var (
 	//ErrUserExists : Generic error for user already existing
 	ErrUserExists = errors.New("error - User Already Exists")
+	//ErrSessionExists : Generic error for session already existing
+	ErrSessionExists = errors.New("error - Session Already Exists")
+
 )
 
 // AddUser : adds a user to the database
@@ -40,7 +44,18 @@ func (handler *SQLHandler) getUser(e string) (*model.User, error) {
 
 // StartSession : Starts a new session based on a session model
 func (handler *SQLHandler) StartSession(s model.Session) (string, error) {
-	return "", nil
+	currentSession := handler.Session()
+	if currentSession != nil {
+		return currentSession.ID, ErrSessionExists
+	}
+	err := createSession(s, handler)
+	if err != nil {
+		currentSession := handler.Session()
+		if currentSession != nil {
+			return currentSession.ID, nil
+		}
+	}
+	return "", api.ErrSomethingWentWrong
 }
 
 // EndSession : Ends the session if the UID firing the ending of the session is the one that created it.
@@ -49,11 +64,22 @@ func (handler *SQLHandler) EndSession(UID string) error {
 }
 
 // Session : Get current session information for the current open session if there is one else return nil
-func (handler *SQLHandler) Session(UID string) *model.Session {
+func (handler *SQLHandler) Session() *model.Session {
+	var currentSession model.Session
+	q := handler.queryManager.getCurrentSession()
+	queryErr := handler.DB.QueryRow(q).Scan(&currentSession.ID, &currentSession.UserID, &currentSession.IsActive, &currentSession.Started, &currentSession.Ended)
+	if currentSession.ID != "" && queryErr == nil {
+		return &currentSession
+	}
 	return nil
 }
 
 // Order : Place an order for the current session and return its order id else return an error
 func (handler *SQLHandler) Order(o model.Order) (string, error) {
 	return "", nil
+}
+
+func createSession(s model.Session, handler *SQLHandler) error {
+	_, err := handler.Exec(fmt.Sprintf("Insert into %s (user_id) values ('%s')", sessionTable(), s.UserID))
+	return err
 }
